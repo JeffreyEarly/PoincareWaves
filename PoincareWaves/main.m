223109be084183303b9e9edbaca983effaced272
Jeffrey J. Early <jearly@nwra.com>
2013-11-19 10:23:51 -0800

    Initial Commit

---
+++ PoincareWaves/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PoincareWaves
+//
+//  Created by Jeffrey J. Early on 11/19/13.
+//  Copyright (c) 2013 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
